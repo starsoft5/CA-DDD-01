dc55afb5d871916c98a2f6caf473dc32dd6a7af7
e-a-11 <apollo_estrella@yahoo.com>
2025-06-26 09:56:18 +0800

    first commit

---
+++ Infrastructure/Migrations/20250624094355_addMobileNoToOrderTbl.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMobileNoToOrderTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Orders",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Orders");
+        }
+    }
+}

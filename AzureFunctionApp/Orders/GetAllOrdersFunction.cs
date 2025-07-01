8e92c002fdbdbab67b20629569a9fd19e3a2667e
e-a-11 <apollo_estrella@yahoo.com>
2025-07-01 09:12:43 +0800

    ci cd pol 1

--- AzureFunctionApp/Orders/GetAllOrdersFunction.cs
+++ AzureFunctionApp/Orders/GetAllOrdersFunction.cs
@@ -31,7 +31,7 @@ public class GetAllOrdersFunction
     [Function("GetAllOrdersFunction")]
     public async Task<IActionResult> Run([Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
     {
-        return new OkObjectResult("[{'name':'I will leave you nor forsake you'}]");
+        return new OkObjectResult("[{'name':'Do not be afraid, trust in Jesus!'}]");
         _logger.LogInformation("Processing request to retrieve all orders.");
 
         var cacheKey = "orders_all";

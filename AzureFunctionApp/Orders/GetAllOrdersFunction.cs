6ff870bb139d6b8ecc17782223b1da3baca77746
e-a-11 <apollo_estrella@yahoo.com>
2025-07-14 13:21:07 +0800

    Test push

--- AzureFunctionApp/Orders/GetAllOrdersFunction.cs
+++ AzureFunctionApp/Orders/GetAllOrdersFunction.cs
@@ -30,7 +30,7 @@ public class GetAllOrdersFunction
     public async Task<IActionResult> Run([Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
     {
         //return new OkObjectResult("[{'name':'God is faithful forever!'}]");
-        _logger.LogInformation("Processing request to retrieve all orders now.");
+        _logger.LogInformation("Processing request to retrieve all orders now again.");
 
         var cacheKey = "orders_all";
         var cached = await _cache.GetStringAsync(cacheKey);
